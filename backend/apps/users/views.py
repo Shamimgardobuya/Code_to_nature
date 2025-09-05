5f5b231d37ebdb9d710f365c52acfd43cd8b5423
merytpeters <merytpeters@gmail.com>
2025-09-05 21:11:34 +0100

    Update: DRF serializer and viewsets for signup and login done

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import CustomerUserSerializer
+from .models import CustomUser
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomerUserSerializer
+    permission_classes = [permissions.AllowAny]
