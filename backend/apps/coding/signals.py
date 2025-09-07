3a9a82ced6c4210ba096f7d615c3f2414276d118
merytpeters <merytpeters@gmail.com>
2025-09-07 19:20:35 +0100

    Add signal to create daily GitHub coding sessions and log creation

--- backend/apps/coding/signals.py
+++ backend/apps/coding/signals.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import CodingSession
+from apps.users.models import Profile
 
 
 @receiver(post_save, sender=CodingSession)
@@ -10,3 +12,17 @@ def add_locked_credits(sender, instance, created, **kwargs):
         profile = instance.user
         profile.locked_credits += instance.credits_awarded
         profile.save()
+
+@receiver(post_save, sender=Profile)
+def create_github_coding_session(sender, instance, created, **kwargs):
+    """Signal to create coding session daily"""
+    if instance.github_username:
+        today = instance.coding_sessions.filter(
+            source='github',
+            created_at__date=datetime.today().date()
+        ).exists()
+        if not today:
+            CodingSession.objects.create(
+                user=instance,
+                source='github'
+            )
